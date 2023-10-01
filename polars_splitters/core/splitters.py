9c2d6da40e966e71d78b95a12ebd89c9060b4781
Jonas M. Miguel <Jonas M. Miguel>
2023-10-01 19:28:41 +0200

    [fix] reimplemented split_into_train_eval ensuring eval set has at least one row per stratum, including validation of inputs and outputs

--- polars_splitters/core/splitters.py
+++ polars_splitters/core/splitters.py
@@ -1,277 +1,206 @@
-from typing import Dict, List, Literal, Tuple, Union, overload
+from typing import Dict, List, Tuple, Union, overload
 
-from polars import DataFrame, Int64, LazyFrame, count, int_range
+from loguru import logger
+from polars import FLOAT_DTYPES, DataFrame, Int64, LazyFrame, count, int_range
+from polars import selectors as cs
 
-from polars_splitters.utils.validators import validate_inputs
+from polars_splitters.utils.type_enforcers import enforce_type
 
 df_pl = DataFrame | LazyFrame
 
-
-@overload
-def _split_into_subsets(
-    as_dict: Literal[True],
-    as_lazy: Literal[True],
-    *args,
-    **kwargs,
-) -> Dict[str, LazyFrame]:
-    ...
+__all__ = [
+    "split_into_train_eval",
+    "split_into_k_folds",
+]
 
 
-@overload
-def _split_into_subsets(
-    as_dict: Literal[True],
-    as_lazy: Literal[False],
-    *args,
-    **kwargs,
-) -> Dict[str, DataFrame]:
-    ...
+def get_suggestion_for_loosening_stratification(func_name: str) -> str:
+    if func_name == "split_into_train_eval":
+        balance_suggestion = "using an `eval_rel_size` closer to 0.5"
+    elif func_name == "split_into_k_folds":
+        balance_suggestion = "using a smaller `k`"
+
+    return f"""
+            Consider:
+            (a) {balance_suggestion},
+            (b) using fewer columns in `stratify_by` columns,
+            (c) disabling stratification altogether (stratify_by=None) or
+            (d) using a larger input dataset df.
+    """
 
 
-@overload
-def _split_into_subsets(
-    as_dict: Literal[False],
-    as_lazy: Literal[True],
-    *args,
-    **kwargs,
-) -> Dict[str, LazyFrame]:
-    ...
+def get_lazyframe_size(df: LazyFrame) -> int:
+    return df.select(count()).collect().item()
 
 
-@overload
-def _split_into_subsets(
-    as_dict: Literal[False],
-    as_lazy: Literal[False],
-    *args,
-    **kwargs,
-) -> Tuple[DataFrame, ...]:
-    ...
+@logger.catch
+def split_into_train_eval(
+    df_lazy: LazyFrame | DataFrame,
+    eval_rel_size: float,
+    stratify_by: str | List[str] = None,
+    shuffle: bool = True,
+    seed: int = 273,
+    validate: bool = True,
+    rel_size_deviation_tolerance: float = 0.1,
+) -> Tuple[LazyFrame, LazyFrame]:
+    r"""
+    Split a dataset into non-overlapping train and eval sets, optionally stratifying by a column or list of columns.
+    It is a wrapper around _split_into_train_eval, containing additional type coercion for the inputs, as well as validation for the inputs and outputs.
+
+    Parameters
+    ----------
+    df_lazy : LazyFrame | DataFrame
+        _description_
+    eval_rel_size : float
+        _description_
+    stratify_by : str | List[str], optional. Defaults to None.
+        _description_
+    shuffle : bool, optional. Defaults to True.
+        _description_
+    seed : int, optional. Defaults to 273.
+        _description_
+    validate : bool, optional. Defaults to True.
+        _description_
+    rel_size_deviation_tolerance : float, optional. Defaults to 0.1.
+        _description_
+
+    Returns
+    -------
+    Tuple[LazyFrame, LazyFrame]
+        _description_
+
+    Raises
+    ------
+    NotImplementedError
+        When trying to stratify by a float column.
+    ValueError
+        When the actual relative size of the eval set deviates from the requested relative size by more than the specified tolerance.
+        Or when the size of the smallest set is smaller than the number of strata (unique row-wise combinations of values in the stratify_by columns).
+
+    Examples
+    --------
+    >>> import polars as pl
+    >>> from polars_splitters.core.splitters import split_into_train_eval
+    >>> df = pl.DataFrame(
+    ...     {
+    ...         "feature_1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    ...         "treatment": [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    ...         "outcome":   [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    ...     }
+    ... )
+    >>> df_train, df_test = split_into_train_eval(df, eval_rel_size=0.3, stratify_by=["treatment", "outcome"], shuffle=True)
+    >>> print(df_train.collect(), df_test.collect(), sep="\n\n")
+    shape: (7, 3)
+    ┌───────────┬───────────┬─────────┐
+    │ feature_1 ┆ treatment ┆ outcome │
+    │ ---       ┆ ---       ┆ ---     │
+    │ f64       ┆ i64       ┆ i64     │
+    ╞═══════════╪═══════════╪═════════╡
+    │ 1.0       ┆ 0         ┆ 0       │
+    │ 3.0       ┆ 0         ┆ 0       │
+    │ 4.0       ┆ 0         ┆ 0       │
+    │ 5.0       ┆ 0         ┆ 0       │
+    │ 6.0       ┆ 1         ┆ 0       │
+    │ 8.0       ┆ 1         ┆ 0       │
+    │ 9.0       ┆ 1         ┆ 1       │
+    └───────────┴───────────┴─────────┘
+
+    shape: (3, 3)
+    ┌───────────┬───────────┬─────────┐
+    │ feature_1 ┆ treatment ┆ outcome │
+    │ ---       ┆ ---       ┆ ---     │
+    │ f64       ┆ i64       ┆ i64     │
+    ╞═══════════╪═══════════╪═════════╡
+    │ 2.0       ┆ 0         ┆ 0       │
+    │ 7.0       ┆ 1         ┆ 0       │
+    │ 10.0      ┆ 1         ┆ 1       │
+    └───────────┴───────────┴─────────┘
+    """
+    # type coercion
+    df_lazy = df_lazy.lazy()
+    stratify_by = enforce_type(stratify_by, list)
+
+    if validate:
+        validate_eval_rel_size_setting(eval_rel_size)
+
+        if stratify_by:
+            # validate stratification dtypes
+            stratification_columns_of_float_type = df_lazy.select(stratify_by).select(cs.by_dtype(FLOAT_DTYPES)).schema
+            if stratification_columns_of_float_type:
+                raise NotImplementedError(
+                    f"""
+                    Attempted to stratify based on float column(s): {stratification_columns_of_float_type}.
+                    This is not currently supported. Consider discretizing the column first or using a different column.
+                    """
+                )
+
+            # validate stratification feasibility (size_input, eval_rel_size, n_strata, stratify_by)
+            input_size = get_lazyframe_size(df_lazy)
+            eval_size = df_lazy.select((eval_rel_size * count()).floor().clip_min(1).cast(Int64)).collect().item()
+            if eval_rel_size <= 0.5:
+                smallest_set, smallest_set_size = ("eval", eval_size)
+            else:
+                smallest_set, smallest_set_size = ("train", input_size - eval_size)
+
+            n_strata = df_lazy.select(stratify_by).collect().n_unique()
+            logger.debug(
+                f"input_size: {input_size}, eval_size: {eval_size}, (input_size - eval_size): {input_size - eval_size}"
+            )
+            logger.debug(
+                f"smallest_set: {smallest_set}, smallest_set_size: {smallest_set_size}, n_strata: {n_strata}, stratify_by: {stratify_by}"
+            )
+            if smallest_set_size < n_strata:
+                f"""
+                Unable to generate the data splits for the data df and the configuration attempted for eval_rel_size and stratify_by.
+                For the stratification to work, the size of the smallest set (currently {smallest_set}: {smallest_set_size})
+                must be at least as large as the number of strata (currently {n_strata}), i.e. the number of unique row-wise combinations of values in the stratify_by columns (currently {stratify_by}).
+
+                {get_suggestion_for_loosening_stratification("split_into_train_eval")}
+                """
+
+    df_train, df_eval = _split_into_train_eval(df_lazy, eval_rel_size, stratify_by, shuffle, seed)
+
+    if validate:
+        eval_rel_size_actual = get_lazyframe_size(df_eval) / input_size
+
+        logger.debug(f"eval_rel_size: {eval_rel_size}, eval_rel_size_actual: {eval_rel_size_actual}")
+
+        if abs(eval_rel_size_actual - eval_rel_size) > rel_size_deviation_tolerance:
+            raise ValueError(
+                f"""
+                The actual relative size of the eval set ({eval_rel_size_actual}) deviates from the requested relative size ({eval_rel_size}) by more than the specified tolerance ({rel_size_deviation_tolerance}).
+
+                {get_suggestion_for_loosening_stratification("split_into_train_eval")}
+                """
+            )
+
+    return (df_train, df_eval)
 
 
-def _split_into_subsets(
-    df: df_pl,
-    rel_sizes: Tuple[float, ...] | Dict[str, float],
+def _split_into_train_eval(
+    df_lazy: LazyFrame,
+    eval_rel_size: float,
     stratify_by: List[str] = None,
     shuffle: bool = True,
-    as_dict: bool = False,
-    as_lazy: bool = False,
     seed: int = 273,
-) -> Tuple[df_pl, ...] | Dict[str, df_pl]:
-    """Split a dataset into non-overlapping len(rel_sizes) subsets.
-
-    Args:
-        df (polars.DataFrame or polars.LazyFrame): The DataFrame to split.
-        rel_sizes (tuple or dict): The relative sizes of the subsets. If a tuple, the sizes are interpreted as proportions
-            of the total number of rows. If a dictionary, the keys are the names of the subsets and the values are the
-            relative sizes. It must sum to 1.
-        stratify_by (list of str, optional): The column names to use for stratification. If None (default), stratification is not
-            performed. Note: Stratification by continuously-valued columns (float) is not currently supported.
-        shuffle (bool, optional): Whether to shuffle the rows before splitting. Defaults to True.
-        as_dict (bool, optional): Whether to return the subsets as a dictionary with the subset names as keys. Defaults
-            to False.
-        as_lazy (bool, optional): Whether to return the subsets as lazy DataFrames. Defaults to False.
-        seed (int, optional): The random seed to use for shuffling. Defaults to 273.
-
-    Returns:
-        tuple or dict: The subsets as DataFrames. If `as_dict` is True, the subsets are returned as a dictionary with
-            the subset names as keys.
-
-    Raises:
-        ValueError: If `rel_sizes` does not sum to one.
-        NotImplementedError: If any column in `stratify_by` is of type float.
-    """
-    rel_sizes_ = rel_sizes
-    if isinstance(rel_sizes, tuple):
-        rel_sizes_ = {f"subset_{i}": rel_size for i, rel_size in enumerate(rel_sizes)}
-
-    df_lazy = df.lazy()
-
+) -> Tuple[LazyFrame, LazyFrame]:
+    """Split a dataset into non-overlapping train and eval sets. Allows for stratification by a column or list of columns."""
     idxs = int_range(0, count())
     if shuffle:
         idxs = idxs.shuffle(seed=seed)
 
-    sizes = {subset: (rel_size * count()).floor().cast(Int64) for subset, rel_size in rel_sizes_.items()}
+    eval_size = (eval_rel_size * count()).floor().clip_min(1).cast(Int64)
 
-    if stratify_by and isinstance(stratify_by, str):
-        stratify_by = [stratify_by]
-
+    if stratify_by:
         idxs = idxs.over(stratify_by)
-        sizes = {k: v.over(stratify_by) for k, v in sizes.items()}
+        eval_size = eval_size.over(stratify_by)
 
-    sizes = {k: v for k, v in sizes.items()}
-    subsets = {subset: None for subset in sizes.keys()}
-    for i, subset in enumerate(subsets.keys()):
-        sizes_already_used = sum(list(sizes.values())[:i])
-        is_subset = (sizes_already_used <= idxs) & (idxs < sizes_already_used + sizes[subset])
-        subsets[subset] = df_lazy.filter(is_subset)
+    is_eval = idxs < eval_size
 
-    if not as_lazy:
-        subsets = {k: v.collect() for k, v in subsets.items()}
+    df_train = df_lazy.filter(~is_eval)
+    df_eval = df_lazy.filter(is_eval)
 
-    if isinstance(rel_sizes, dict) and as_dict:
-        return subsets
-    else:
-        return tuple(subsets.values())
-
-
-@overload
-def split_into_train_val_test(
-    as_dict: Literal[True],
-    as_lazy: Literal[True],
-    *args,
-    **kwargs,
-) -> Dict[str, LazyFrame]:
-    ...
-
-
-@overload
-def split_into_train_val_test(
-    as_dict: Literal[True],
-    as_lazy: Literal[False],
-    *args,
-    **kwargs,
-) -> Dict[str, DataFrame]:
-    ...
-
-
-@overload
-def split_into_train_val_test(
-    as_dict: Literal[False],
-    as_lazy: Literal[False],
-    *args,
-    **kwargs,
-) -> Tuple[DataFrame, DataFrame, DataFrame]:
-    ...
-
-
-@overload
-def split_into_train_val_test(
-    as_dict: Literal[False],
-    as_lazy: Literal[True],
-    *args,
-    **kwargs,
-) -> Tuple[LazyFrame, LazyFrame, LazyFrame]:
-    ...
-
-
-@validate_inputs
-def split_into_train_val_test(
-    df: df_pl,
-    rel_sizes: Tuple[float, float, float],
-    stratify_by: List[str] = None,
-    shuffle: bool = True,
-    as_dict: bool = False,
-    as_lazy: bool = False,
-    seed: int = 273,
-) -> Tuple[df_pl, df_pl, df_pl] | Dict[str, df_pl]:
-    """Split a dataset into non-overlapping train, validation, and test sets. Allows for stratification by a column or list of columns.
-
-    This is a specific case of `split_into_subsets` in which the `rel_sizes` are limited to 3 and are interpreted as `(train_rel_size, val_rel_size, test_rel_size)`.
-
-    Args:
-        df (polars.DataFrame or polars.LazyFrame): The DataFrame to split.
-        rel_sizes (tuple): A tuple of 3 rel_sizes representing the relative sizes of the subsets train, validation and test, respectively. The rel_sizes must sum to 1.
-        stratify_by (list of str, optional): The column names to use for stratification. If None (default), stratification is not
-            performed. Note: Stratification by continuously-valued columns (float) is not currently supported.
-        shuffle (bool, optional): Whether to shuffle the rows before splitting. Defaults to True.
-        as_dict (bool, optional): Whether to return the subsets as a dictionary with the subset names as keys. Defaults
-            to False.
-        as_lazy (bool, optional): Whether to return the subsets as lazy DataFrames. Defaults to False.
-        seed (int, optional): The random seed to use for shuffling. Defaults to 273.
-
-    Returns:
-        tuple or dict: The subsets as DataFrames. If `as_dict` is True, the subsets are returned as a dictionary with
-            the subset names as keys.
-
-    Raises:
-        ValueError: If `rel_sizes` does not sum to one.
-        NotImplementedError: If any column in `stratify_by` is of type float, which is not currently supported.
-    """
-
-    subsets = _split_into_subsets(df, rel_sizes, stratify_by, shuffle, as_dict=False, as_lazy=as_lazy, seed=seed)
-    if as_dict:
-        return {k: v for k, v in zip(("train", "val", "test"), subsets)}
-    else:
-        return subsets
-
-
-@overload
-def split_into_train_eval(
-    as_dict: Literal[True],
-    as_lazy: Literal[True],
-    *args,
-    **kwargs,
-) -> Dict[str, LazyFrame]:
-    ...
-
-
-@overload
-def split_into_train_eval(
-    as_dict: Literal[True],
-    as_lazy: Literal[False],
-    *args,
-    **kwargs,
-) -> Dict[str, DataFrame]:
-    ...
-
-
-@overload
-def split_into_train_eval(
-    as_dict: Literal[False],
-    as_lazy: Literal[False],
-    *args,
-    **kwargs,
-) -> Tuple[DataFrame, DataFrame]:
-    ...
-
-
-@overload
-def split_into_train_eval(
-    as_dict: Literal[False],
-    as_lazy: Literal[True],
-    *args,
-    **kwargs,
-) -> Tuple[LazyFrame, LazyFrame]:
-    ...
-
-
-@validate_inputs
-def split_into_train_eval(
-    df: df_pl,
-    eval_rel_size: float,
-    stratify_by: List[str] = None,
-    shuffle: bool = True,
-    as_dict: bool = False,
-    as_lazy: bool = False,
-    seed: int = 273,
-) -> Tuple[df_pl, df_pl] | Dict[str, df_pl]:
-    """Split a dataset into non-overlapping train and eval sets. Allows for stratification by a column or list of columns.
-
-    This is a specific case of `split_into_subsets` in which the `rel_sizes` represent`(train_rel_size, eval_rel_size)`, with train_rel_size = 1 - eval_rel_size.
-
-    Args:
-        df (polars.DataFrame or polars.LazyFrame): The DataFrame to split.
-        eval_rel_size (float): A decimal representing the relative size of the evaluation subset.
-        stratify_by (list of str, optional): The column names to use for stratification. If None (default), stratification is not
-            performed. Note: Stratification by continuously-valued columns (float) is not currently supported.
-        shuffle (bool, optional): Whether to shuffle the rows before splitting. Defaults to True.
-        as_dict (bool, optional): Whether to return the subsets as a dictionary with the subset names as keys. Defaults
-            to False.
-        as_lazy (bool, optional): Whether to return the subsets as lazy DataFrames. Defaults to False.
-        seed (int, optional): The random seed to use for shuffling. Defaults to 273.
-
-    Returns:
-        tuple or dict: The subsets as DataFrames. If `as_dict` is True, the subsets are returned as a dictionary with
-            the subset names as keys.
-
-    Raises:
-        NotImplementedError: If any column in `stratify_by` is of type float, which is not currently supported.
-    """
-    rel_sizes = (1 - eval_rel_size, eval_rel_size)
-    subsets = _split_into_subsets(df, rel_sizes, stratify_by, shuffle, as_dict=False, as_lazy=as_lazy, seed=seed)
-    if as_dict:
-        return {k: v for k, v in zip(("train", "eval"), subsets)}
-    else:
-        return subsets
+    return (df_train, df_eval)
 
 
 def split_into_k_folds(
